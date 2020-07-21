21a1fd556866c3d2f78c64915382c8c3226b74ac
charanjarwal <charanjarwal@LAPTOP-O72MBBSG.localdomain>
2020-07-21 23:21:46 +0530

    js_lab1

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-// Important Note - No Built-in functions to be used
-// Progression 1: Names and Input
-
-// 1.1 Create a variable `ProGrad-1` with the driver's name.
-// 1.2 Print `"The driver's name is XXXX"`.
-// 1.3 Create a variable `ProGrad-2` with the navigator's name.
-// 1.4 Print `"The navigator's name is YYYY"`.
-
-// Progression 2: Control Statements - 1
-// 2.1. Depending on which name is longer, print:
-// - The driver has the longest name, it has XX characters. or
-// - It seems that the navigator has the longest name, it has XX characters. or
-// - Wow, you both have equally long names, XX characters!.
-
-// 2.2. Check if the string contains vowels or not.
-// - If it contains vowels, print the name, and also print the vowel letters along with the vowel index. or
-// - print no vowels
-// - for example. In String ProGrad - o and a are vowels. Print ProGrad o a 2 5. 
-
-// 2.3. Check if the string contains uppercase and lowercase characters Xx
-// - Print the number of upper case characters
-// - Print the number of lower case characters
+function print() {
+    var ProGrad2 = "Charan";
+    var ProGrad1 = "Chaitra";
+    console.log("Drivers Name is " + ProGrad2);
+    console.log("Navigators Name is " + ProGrad1);
+    var count = 0;
+    var count1 = 0;
 
 
-// Progression 3: Control Statements - 2
-// 3.1 Print all the characters of the driver's name, separated by a space and in capitals i.e. "ProGrad"
+    for (i = 0; ProGrad1.charAt(i) != '\0'; i++) {
+        count++;
+    }
+    for (i = 0; ProGrad2.charAt(i) != '\0'; i++) {
+        count1++
+    }
+    if (count > count1) {
+        console.log("prograd1 has the longest name and it has characters");
+    } else if (count < count1) {
+        console.log("prograd2 has the longest name and it has" + count + "characters");
+    } else {
+        console.log("both have " + count + "these many letters");
+    }
 
-// 3.2 Print all the characters of the navigator's name, in reverse order. i.e. "darGorP"
+    for (var i = 0; ProGrad1.charAt(i) != '\0'; i++) {
+        if (ProGrad1.charAt(i) == 'a' || ProGrad1.charAt(i) == 'e' || ProGrad1.charAt(i) == 'i' || ProGrad1.charAt(i) == 'o' || ProGrad1.charAt(i) == 'u') {
+            console.log(ProGrad1.char(i) + " in Prograd1 which is present at location " + i);
+        } else {
+            console.log("no vowels");
+        }
+    }
 
-// 3.3 Merge both the characters such that driver is followed by Navigator like "ProGrad FACEPrep"
-// - Now bring the FACEPrep to the start and send ProGrad to the back like "FACEPrep ProGrad"
+    for (var i = 0; ProGrad2.charAt(i) != '\0'; i++) {
+        if (ProGrad2.charAt(i) == 'a' || ProGrad2.charAt(i) == 'e' || ProGrad2.charAt(i) == 'i' || ProGrad2.charAt(i) == 'o' || ProGrad2.charAt(i) == 'u') {
+            console.log(ProGrad2.char(i) + " in Prograd1 which is present at location " + i);
+        } else {
+            console.log("no vowels");
+        }
+    }
+    var uppercase1 = 0;
+    var lowercase1 = 0;
+    for (var i = 0; ProGrad1.charAt(i) != '\0'; i++) {
+        var letter = ProGrad2.charAt(i)
+        if (ProGrad1.charAt(i) == letter.toUpperCase() || ProGrad1.charAt(i) != letter.toLowerCase()) {
+            uppercase1++;
+        } else if (ProGrad1.charAt(i) == letter.toLowerCase() || ProGrad1.charAt(i) != letter.toUpperCase()) {
+            lowercase1++;
+        }
 
-// 3.3 Depending on the lexicographic order of the strings, print:
-// - The driver's name goes first.
-// - Yo, the navigator goes first definitely.
-// - What?! You both have the same name?
+    }
 
-// Bonus Time!
-// Bonus 1:
-// Go to lorem ipsum generator and:
-// Generate 3 paragraphs. Store the text in a variable type of string.
-// Make your program count the number of words in the string.
-// Make your program count the number of times the Latin word et appears.
-// Bonus 2:
-// Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome. Here are some examples of palindromes:
 
-// "A man, a plan, a canal, Panama!"
-// "Amor, Roma"
-// "race car"
-// "stack cats"
-// "step on no pets"
-// "taco cat"
-// "put it up"
-// "Was it a car or a cat I saw?" and "No 'x' in Nixon".
 
-// Hint: If you use Google to help you to find solution to this iteration, you might run into some solutions that use advanced string or array methods (such as join(), reverse(), etc.). However, try to apply the knowledge you currently have since you can build pretty nice solution with just using for loop, if-else statements with some break and continue... Just sayin' 
+    for (var i = 0; ProGrad2.charAt(i) != '\0'; i++) {
+        var letter = ProGrad2.charAt(i)
+        if (ProGrad2.charAt(i) == letter.toUpperCase() || ProGrad2.charAt(i) != letter.toLowerCase()) {
+            uppercase1++;
+        } else if (ProGrad2.charAt(i) == letter.toLowerCase() || ProGrad2.charAt(i) != letter.toUpperCase()) {
+            lowercase1++;
+        }
+
+    }
+
+
+    console.log(uppercase1 + "" + lowercase1);
+    var count = ProGrad2.length();
+    for (i = count; i > 0; i--) {
+        console.log(ProGrad2.charAt(i));
+    }
+}
